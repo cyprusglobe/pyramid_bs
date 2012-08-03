6b4920b4d455038de5fa7856be0a5423dae051ac
Kelvin <kjones@lobo.net>
2012-08-03 14:10:25 -0600

    - changed the forbidden flash message

--- pyramid_bs/views/__init__.py
+++ pyramid_bs/views/__init__.py
@@ -6,7 +6,7 @@ from pyramid.security import authenticated_userid
 def forbidden(request):
     # do not allow a user to login if they are already logged in
     if authenticated_userid(request):
-        request.session.flash('You cannot edit users your lacking Secured Permissions Your Currently only have Basic Permissions', 'error')
+        request.session.flash('You cannot access this because your lacking Secured Permissions You Currently only have Basic Permissions', 'error')
         location = request.route_url('index')
         #return HTTPForbidden()
     else:
